01232224e1ea2e22d2dc587ae7b487a64f8c1fb5
SHEFFmachine <sheff@sheffmachine.com>
2018-07-31 20:19:03 -0700

    start setting up the hapi graphql boilterplate

---
+++ server/svr.js
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require('apollo-server-hapi')
+const Hapi = require('hapi')
+const typeDefs = require('./schema/schema')
+const resolvers = require('./resolvers/resolvers')
+
+async function StartServer() {
+  const server = new ApolloServer({ typeDefs, resolvers })
+
+  const app = new Hapi.server({
+    port: 4000
+  })
+
+  await server.applyMiddleware({
+    app
+  })
+
+  await server.installSubscriptionHandlers(app.listener)
+
+  await app.start()
+}
+
+StartServer().catch(error => console.log(error))
